28ec143e6dd8812efb7f149c768f8cea7e7778ae
YuyuanW <1413733614@qq.com>
2022-10-06 16:21:02 +0800

    router的routes和通用组件sidebar的布局

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <sidebar></sidebar>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import sidebar from "@/components/Sidebar.vue";
+
+export default {
+  name: "app",
+  components: {
+    sidebar,
+  },
+};
+</script>
+
+<style>
+@import "//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css";
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-nav {
-  padding: 30px;
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #eee;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+a {
+  text-decoration: none;
+  color: #444;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+ul,
+li {
+  list-style: none;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0px 0px 2px 0px #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+html,
+body,
+#app {
+  height: 100%;
+}
+#app {
+  display: flex;
+  align-items: stretch;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '@/components/Login'
+import Notebooks from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +17,23 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/notebooks',
+    component: Notebooks
+  },
+  {
+    path: '/note/:noteId',
+    component: NoteDetail
+  },
+  {
+    path: '/trash/:noteId',
+    component: TrashDetail
   }
 ]
 
