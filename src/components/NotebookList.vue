8b4f6dc6975babb2ee4dbf36ecc273ca50bad023
YuyuanW <1413733614@qq.com>
2022-10-23 22:26:33 +0800

    完成笔记本列表页的制作

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="detail" id="notebook-list">
     <header>
-      <a href="#" class="btn"><i class="iconfont icon-plus"></i>新建笔记本</a>
+      <a href="#" class="btn" @click="onCreate"
+        ><i class="iconfont icon-plus"></i>新建笔记本</a
+      >
     </header>
     <main>
       <div class="layout">
-        <h3>笔记本列表(10)</h3>
+        <h3>笔记本列表({{ notebooks.length }})</h3>
+
         <div class="book-list">
-          <a href="#" class="notebook">
+          <router-link
+            v-for="notebook in notebooks"
+            to="/note/1"
+            class="notebook"
+            :key="notebook.id"
+          >
             <div>
-              <span class="iconfont icon-book"></span>笔记本标题1
-              <span>3</span> <span class="action">编辑</span>
-              <span class="action">删除</span>
-              <span class="date">3天前</span>
+              <span class="iconfont icon-book">{{ notebook.title }}</span>
+              <span>{{ notebook.noteCounts }}</span>
+              <span class="action" @click.stop.prevent="onEdit(notebook)"
+                >编辑</span
+              >
+              <span class="action" @click.stop.prevent="onDelete(notebook)"
+                >删除</span
+              >
+              <span class="date">{{ notebook.friendlyCreatedAt }}</span>
             </div>
-          </a>
-          <a href="#" class="notebook">
-            <div>
-              <span class="iconfont icon-book"></span>笔记本标题2
-              <span>1</span> <span class="action">编辑</span>
-              <span class="action">删除</span>
-              <span class="date">5天前</span>
-            </div>
-          </a>
+          </router-link>
         </div>
       </div>
     </main>
@@ -31,10 +36,14 @@
 
 <script>
 import Auth from "@/apis/auth";
+import Notebooks from "@/apis/notebooks";
+import { friendlyDate } from "@/helpers/util";
+window.Notebooks = Notebooks;
 export default {
   data() {
     return {
       msg: "笔记本列表加载成功",
+      notebooks: [],
     };
   },
   created() {
@@ -43,6 +52,46 @@ export default {
         this.$router.push({ path: "/login" });
       }
     });
+    Notebooks.getAll().then((res) => {
+      this.notebooks = res.data;
+    });
+  },
+  methods: {
+    onCreate() {
+      let title = window.prompt("请输入新建笔记本的标题：");
+      if (title.trim() === "") {
+        window.alert("笔记本名不能为空，请重新创建");
+        return;
+      }
+      Notebooks.addNotebook({ title }).then((res) => {
+        res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt);
+        this.notebooks.unshift(res.data);
+        window.alert(res.msg);
+      });
+    },
+    onEdit(notebook) {
+      let title = window.prompt(`将笔记本${notebook.title}更改为：`);
+      if (title.trim() === "") {
+        window.alert("笔记本名不能为空");
+        return;
+      } else if (title === notebook.title) {
+        window.alert("笔记本名请更改为不同的名字");
+        return;
+      }
+      Notebooks.updateNotebook(notebook.id, { title }).then((res) => {
+        notebook.title = title;
+        window.alert(res.msg);
+      });
+    },
+    onDelete(notebook) {
+      let isConfirm = window.confirm(`确定删除${notebook.title}吗？`);
+      if (isConfirm) {
+        Notebooks.deleteNotebook(notebook.id).then((res) => {
+          this.notebooks.splice(this.notebooks.indexOf(notebook), 1);
+          window.alert(res.msg);
+        });
+      }
+    },
   },
 };
 </script>
@@ -80,6 +129,7 @@ main {
   height: calc(100vh - 46px);
   overflow: scroll;
 }
+
 .book-list {
   margin-top: 10px;
   font-size: 14px;
@@ -92,6 +142,10 @@ main .book-list span {
   font-size: 12px;
   font-weight: bold;
   color: #b3c0c8;
+  /* color: black; */
+}
+main .book-list .icon-book {
+  color: rgb(12, 12, 12);
 }
 main .date,
 main .action {
