ff7fd8bd590ce599938e38bf446d493a81e5ede7
YuyuanW <1413733614@qq.com>
2022-10-28 00:27:14 +0800

    完成回收站页面的html和

--- src/components/TrashDetail.vue
+++ src/components/TrashDetail.vue
@@ -1,15 +1,52 @@
 <template>
   <div id="trash-detail">
-    <h1>{{ msg }} : {{ $route.params.noteId }}</h1>
+    <div class="trash-side">
+      <header>回收站</header>
+      <main>
+        <div>
+          <span>更新时间</span>
+          <span>标题</span>
+        </div>
+        <div class="trash-note">
+          <ul>
+            <li v-for="note in notes" :key="note.id">
+              <router-link :to="`/trash?noteId=${note.id}`">
+                <span>{{ note.updatedAtFriendly }}</span>
+                <span>{{ note.title }}</span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </div>
+    <div class="trash-show">
+      <header>
+        <span>回收数量：{{ this.notes.length }}</span>
+        <span>创建日期:{{ this.curNote.createdAtFriendly }}</span>
+        <span>更新日期:{{ this.curNote.updatedAtFriendly }}</span>
+        <span>所属笔记本:{{ this.curNote.notebookId }}</span>
+        <button>彻底删除</button>
+        <button>恢复</button>
+      </header>
+      <main>
+        <div class="trash-noteDetail">
+          <div class="trash-title">{{ this.curNote.title }}</div>
+          <div class="trash-content">{{ this.curNote.content }}</div>
+        </div>
+        <div class="trash-tips">当前回收站为空</div>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
 import Auth from "@/apis/auth";
+import Trash from "@/apis/trash";
 export default {
   data() {
     return {
-      msg: "回收站笔记详情页",
+      notes: [],
+      curNote: {},
     };
   },
   created() {
@@ -18,12 +55,15 @@ export default {
         this.$router.push({ path: "/login" });
       }
     });
+    Trash.getAll().then((res) => {
+      this.notes = res.data;
+      this.curNote = this.notes[0];
+      if (this.curNote.id) {
+        this.$router.push({ path: `/trash?noteId=${this.curNote.id}` });
+      }
+    });
   },
 };
 </script>
 
-<style scoped>
-h1 {
-  color: blue;
-}
-</style>
+<style scoped></style>
